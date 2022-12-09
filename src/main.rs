0a48c14c3a4bfc94487b78b8e25d71b7ca289f49
Lord-sarcastic <adeoti.15.jude@gmail.com>
2022-12-09 23:50:15 +0100

    Chore: Ran formating on code

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use actix_web::{HttpRequest, Responder};
 use std::net::TcpListener;
 use zero::run;
 
-
 async fn _greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {name}")
@@ -10,7 +9,6 @@ async fn _greet(req: HttpRequest) -> impl Responder {
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000")
-        .expect("Failed to bind to a random port");
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to a random port");
     run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,11 @@
 use std::net::TcpListener;
 
-
 #[tokio::test]
 async fn health_check_works() {
     let address = spawn_app();
     let client = reqwest::Client::new();
     let response = client
-    .get(format!("{}/health_check", &address))
+        .get(format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -16,8 +15,7 @@ async fn health_check_works() {
 }
 
 fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind to a random port");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to a random port");
     let port = listener.local_addr().unwrap().port();
     let server = zero::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
