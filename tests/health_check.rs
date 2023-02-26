954cd71531d755d19436b86d177026aea75c5ef2
Lord-sarcastic <adeoti.15.jude@gmail.com>
2023-02-27 00:49:34 +0100

    Buggy: Saved application state

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,14 @@
 use std::net::TcpListener;
 
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to a random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero::run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    println!("Address: {}", &port);
+    format!("http://127.0.0.1:{}", port)
+}
+
 #[tokio::test]
 async fn health_check_works() {
     let address = spawn_app();
@@ -14,11 +23,46 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to a random port");
-    let port = listener.local_addr().unwrap().port();
-    let server = zero::run(listener).expect("Failed to bind address");
-    let _ = tokio::spawn(server);
-    println!("Address: {}", &port);
-    format!("http://127.0.0.1:{}", port)
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to xqt request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let data_and_message = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (body, error_msg) in data_and_message {
+        let response = client
+            .post(format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to xqt request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_msg
+        );
+    }
 }
