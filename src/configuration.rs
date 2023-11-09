7f6df86f7115656b17f4e931e8d4abe1078816b4
lordsarcastic <>
2023-11-09 15:56:55 +0100

    Feature: Implemented configuration file

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,1 +1,36 @@
+use config::{File, FileFormat};
 
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub host: String,
+    pub name: String,
+    pub port: u16
+}
+
+impl DatabaseSettings {
+    pub fn connection_string (&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:${}/${}",
+            self.username,
+            self.password,
+            self.host,
+            self.port,
+            self.name
+        )
+    }
+}
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let settings = config::Config::builder()
+        .add_source(File::new("configuration", FileFormat::Yaml))
+        .build()?;
+
+    settings.try_deserialize()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use actix_web::{HttpRequest, Responder};
 use std::net::TcpListener;
-use zero::startup::run;
+use zero::{startup::run, configuration::get_configuration};
 
 async fn _greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -9,6 +9,10 @@ async fn _greet(req: HttpRequest) -> impl Responder {
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to a random port");
+    let config = get_configuration()
+        .expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", config.application_port);
+    let listener = TcpListener::bind(address)
+        .expect("Failed to bind to a random port");
     run(listener)?.await
 }
